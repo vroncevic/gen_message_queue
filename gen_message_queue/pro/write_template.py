2f9449f048b4f1154668e9733931af0c508e0d45
Vladimir <elektron.ronca@gmail.com>
2024-07-08 23:24:47 +0200

    [gen_message_queue] Updated ATS CLI mechanism, docs

--- gen_message_queue/pro/write_template.py
+++ gen_message_queue/pro/write_template.py
@@ -21,7 +21,7 @@ Info
 '''
 
 import sys
-from typing import List, Dict
+from typing import List, Dict, Optional
 from datetime import date
 from os import getcwd, chmod, mkdir
 from string import Template
@@ -39,7 +39,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = '(C) 2024, https://vroncevic.github.io/gen_message_queue'
 __credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
 __license__ = 'https://github.com/vroncevic/gen_message_queue/blob/dev/LICENSE'
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -75,7 +75,7 @@ class WriteTemplate(FileCheck):
     def write(
         self,
         template_content: Dict[str, str],
-        pro_name: str | None,
+        pro_name: Optional[str],
         verbose: bool = False
     ) -> bool:
         '''
@@ -84,15 +84,15 @@ class WriteTemplate(FileCheck):
             :param template_content: Template content
             :type template_content: <Dict[str, str]>
             :param pro_name: Project name | None
-            :type pro_name: <str> | <NoneType>
+            :type pro_name: <Optional[str]>
             :param verbose: Enable/Disable verbose option
             :type verbose: <bool>
             :return: True (success operation) | False
             :rtype: <bool>
             :exception: ATSTypeError | ATSValueError
         '''
-        error_msg: str | None = None
-        error_id: int | None = None
+        error_msg: Optional[str] = None
+        error_id: Optional[int] = None
         error_msg, error_id = self.check_params([
             ('dict:template_content', template_content),
             ('str:pro_name', pro_name)
--- tests/gen_message_queue_test.py
+++ tests/gen_message_queue_test.py
@@ -37,7 +37,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = '(C) 2024, https://vroncevic.github.io/gen_message_queue'
 __credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
 __license__ = 'https://github.com/vroncevic/gen_message_queue/blob/dev/LICENSE'
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -100,17 +100,6 @@ class GenMessageQueueTestCase(TestCase):
         sys.argv.insert(3, 'posix')
         generator: GenMessageQueue = GenMessageQueue()
         self.assertTrue(generator.process())
-
-    def test_tool_not_operational(self) -> None:
-        '''Test not operational'''
-        sys.argv.clear()
-        sys.argv.insert(0, '-n')
-        sys.argv.insert(1, 'fresh')
-        sys.argv.insert(2, '-t')
-        sys.argv.insert(3, 'posix')
-        generator: GenMessageQueue = GenMessageQueue()
-        generator.tool_operational = False
-        self.assertFalse(generator.process())
 
     def test_pro_already_exists(self) -> None:
         '''Test pro already exists'''
